e3a0b5bb04587f2422ffd4f6d609c66560c7c618
Salikh Magomedov <sm.saarus@gmail.com>
2020-02-18 20:52:42 +0300

    Move Object With Mouse

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private float screenWidthInUnits = 16f;
+
+    private void Update() => transform.position = new Vector2(Input.mousePosition.x / Screen.width * screenWidthInUnits, transform.position.y);
+}
