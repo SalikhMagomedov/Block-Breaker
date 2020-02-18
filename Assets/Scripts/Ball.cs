6847dc136021cd67e28eba845bfb1d5debff5594
Salikh Magomedov <sm.saarus@gmail.com>
2020-02-18 21:21:05 +0300

    Distance As A Vector2

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private Paddle paddle;
+
+    private Vector2 paddleToBallVector;
+
+    private void Start() => paddleToBallVector = transform.position - paddle.transform.position;
+
+    private void Update() => transform.position = new Vector2(paddle.transform.position.x, paddle.transform.position.y) + paddleToBallVector;
+}
