9dc14b6175241c49cd8073a02010f281073496c2
Salikh Magomedov <sm.saarus@gmail.com>
2020-02-18 21:39:09 +0300

    Launch The Ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,12 +1,36 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     [SerializeField] private Paddle paddle;
+    [SerializeField] private Vector2 push = new Vector2(2f, 15f);
 
     private Vector2 paddleToBallVector;
+    private Rigidbody2D rb;
+    private bool hasStarted = false;
+
+    private void Awake() => rb = GetComponent<Rigidbody2D>();
 
     private void Start() => paddleToBallVector = transform.position - paddle.transform.position;
 
-    private void Update() => transform.position = new Vector2(paddle.transform.position.x, paddle.transform.position.y) + paddleToBallVector;
+    private void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LockBallToPaddle() => transform.position = new Vector2(paddle.transform.position.x, paddle.transform.position.y) + paddleToBallVector;
+    
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = push;
+            hasStarted = true;
+        }
+    }
 }
