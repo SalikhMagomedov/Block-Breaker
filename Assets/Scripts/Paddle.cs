c6972a17eabb8cd1e0c9b5788be4bd582722c505
Salikh Magomedov <sm.saarus@gmail.com>
2020-02-18 21:05:42 +0300

    Limit With Mathf.Clamp()

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,7 +2,11 @@
 
 public class Paddle : MonoBehaviour
 {
+    [SerializeField] private float minX = 1f;
+    [SerializeField] private float maxX = 15f;
     [SerializeField] private float screenWidthInUnits = 16f;
 
-    private void Update() => transform.position = new Vector2(Input.mousePosition.x / Screen.width * screenWidthInUnits, transform.position.y);
+    private void Update() => transform.position = new Vector2(
+        Mathf.Clamp(Input.mousePosition.x / Screen.width * screenWidthInUnits, minX, maxX),
+        transform.position.y);
 }
