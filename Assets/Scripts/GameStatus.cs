adacbba2072247c19159ddb022070f4e78cfff61
Salikh Magomedov <sm.saarus@gmail.com>
2020-02-23 23:04:53 +0300

    Using Time.timeScale

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    [SerializeField][Range(0f, 10f)] private float gameSpeed = 1f;
+
+    // Update is called once per frame
+    void Update() => Time.timeScale = gameSpeed;
+}
