12b101b51f681f01536be11be835bd8c952e0c70
Salikh Magomedov <sm.saarus@gmail.com>
2020-02-23 23:29:51 +0300

    Display Player Score

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     [SerializeField][Range(0f, 10f)] private float gameSpeed = 1f;
     [SerializeField] private int pointsPerBlockDestroyed = 83;
+    [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private int currentScore = 0;
+
+    private void Awake() => scoreText = FindObjectOfType<TextMeshProUGUI>();
+
+    private void Start() => UpdateScoreText();
 
     // Update is called once per frame
     void Update() => Time.timeScale = gameSpeed;
 
-    public void AddToScore() => currentScore += pointsPerBlockDestroyed;
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = currentScore.ToString();
+    }
 }
