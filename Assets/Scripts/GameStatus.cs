cde642743fb357668b6a996acc1dd4ab53eeee14
Salikh Magomedov <sm.saarus@gmail.com>
2020-02-23 23:53:50 +0300

    Singleton Pattern Bug Fix

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -13,6 +13,7 @@ public class GameStatus : MonoBehaviour
         int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
         if (gameStatusCount > 1)
         {
+            gameObject.SetActive(false);
             Destroy(gameObject);
         }
         else
