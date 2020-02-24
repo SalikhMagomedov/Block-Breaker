a961d8d1e1c71f61ca23827ae06983cfb70e40ec
Salikh Magomedov <sm.saarus@gmail.com>
2020-02-24 19:33:00 +0300

    Block Damage Levels

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,9 +4,11 @@ public class Block : MonoBehaviour
 {
     [SerializeField] private AudioClip breakSound;
     [SerializeField] private GameObject blockSparklesVfx;
+    [SerializeField] private int maxHits;
 
     private Level level;
     private GameSession gameStatus;
+    private int timesHit;
 
     private void Awake()
     {
@@ -26,13 +28,18 @@ public class Block : MonoBehaviour
     {
         if (CompareTag("Breakable"))
         {
-            AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
-            var sparkles = Instantiate(blockSparklesVfx, transform.position, transform.rotation);
-            Destroy(sparkles, 1f);
-            level.BlockDestroyed();
-            gameStatus.AddToScore();
+            timesHit++;
+            if (timesHit >= maxHits)
+            {
+                AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+                var sparkles = Instantiate(blockSparklesVfx, transform.position, transform.rotation);
+                Destroy(sparkles, 1f);
 
-            Destroy(gameObject);
+                level.BlockDestroyed();
+                gameStatus.AddToScore();
+
+                Destroy(gameObject);
+            }
         }
     }
 }
